981ff9fad9e5481de17c49973300bd2b360b2a15
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-06 15:05:47 +0900

    개인 보안 노트 서비스 initial commit

---
+++ security-note-service/src/main/java/com/example/securitynoteservice/SecurityNoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitynoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityNoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityNoteServiceApplication.class, args);
+	}
+
+}
---
+++ security-note-service/src/test/java/com/example/securitynoteservice/SecurityNoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitynoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityNoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
